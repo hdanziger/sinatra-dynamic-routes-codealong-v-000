df602840b8d61d0c98f53a1dc5cbc634b97a1f25
Hannah Danziger <hdanziger8@gmail.com>
2017-11-09 19:07:14 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -18,7 +18,7 @@ class App < Sinatra::Base
     "Goodbye, #{@user_name}."
   end
 
-  get "goodbye/:name" do
+  get '/goodbye/:name' do
     @user_name = params[:name]
     "Goodbye, #{@user_name}."
   end
